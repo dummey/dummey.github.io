917936566cb91fc4d97e4801404b093485ca4b64
Ricky Ng <dummey@gmail.com>
2015-08-14 22:43:08 -0500

    Fuck this grid =(

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(window).load( function() {
+
+    $('.grid').masonry({
+        "itemSelector": ".grid-item",
+        "columnWidth": ".grid-sizer",
+        "percentPosition": true,
+    });
+
+});
